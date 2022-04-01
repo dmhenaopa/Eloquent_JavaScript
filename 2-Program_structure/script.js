13a3744d974b97e7de46b588970e4ff0fa5499f8
Diana Henao <dimhenaopa@gmail.com>
2022-03-31 21:59:24 -0500

    :construction: Add some test from chapter 2

--- 2-Program_structure/script.js
+++ 2-Program_structure/script.js
@@ -29,3 +29,73 @@ while (number <= 10) {
     console.log(number);
     number++;
 }
+
+/* EXERCISES */
+// Looping a triangle
+let toPrint = '#';
+for (let i = 0; i <= 6; i++) {
+    console.log(toPrint);
+    toPrint += '#';
+}
+
+// FizzBuzz
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+    
+}
+
+
+// Ternary operator
+for (let i = 0; i <= 100; i++) {
+    ((i % 3 === 0) && (i % 5 === 0)) ? console.log('FizzBuzz') :
+        (i % 3 === 0) ? console.log('Fizz') :
+            (i % 5 === 0) ? console.log('Buzz'): console.log(i);
+}
+
+// Chessboard
+const even = ' # # # #';
+const odd = '# # # # ';
+
+for (let i = 0; i <= 7; i++) {
+    if (i % 2 === 0) {
+        console.log(even);
+    } else {
+        console.log(odd);
+    }
+}
+
+const size = 8;
+for (let i = 0; i < size; i++) {
+    let toPrint = "";
+    for (let j = 0; j < size; j++) {
+        if (i % 2 === 0 && j % 2 === 0) {
+            toPrint += " ";
+        } else if (i % 2 === 0 && j % 2 !== 0) {
+            toPrint += "#";
+        } else if (i % 2 !== 0 && j % 2 === 0) {
+            toPrint += "#";
+        }
+        else if (i % 2 !== 0 && j % 2 !== 0) {
+            toPrint += " ";
+        }
+    }
+    console.log(toPrint);
+}
+
+// Ternary operator
+const sizeTernary = 8;
+for (let i = 0; i < sizeTernary; i++) {
+    let toPrint = "";
+    for (let j = 0; j < sizeTernary; j++) {
+        ((i % 2 === 0 && j % 2 === 0) || (i % 2 !== 0 && j % 2 !== 0)) ? toPrint += " " : toPrint += "#";
+    }
+    console.log(toPrint);
+}
